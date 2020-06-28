00045d26352337bc846460c09d4db8cfda4e77d2
sahil mulla <sahilmulla199@gmail.com>
2020-06-28 14:41:49 +0530

    added comment

--- Odd_Even.java
+++ Odd_Even.java
@@ -3,19 +3,23 @@ package sahil.mulla.sahil;
 public class Odd_Even {
     public static void main(String[] args) {
         int n=5,ev=0,od=0;
+		// there is formula for finding sum of first n odd number and even number
+		// for even = n*(n+1)  for odd=n*n
+		// so our first task is to find out how many even and odd numbers are there in between 1 to n 
         if(n%2==0)
         {
           ev=n/2;
-          od=n/2;
+          od=n/2;   // if n is even then there is n/2 even numbers and n/2 odd numbers 
 
         }
         else {
 
-            ev=n/2;
+            ev=n/2;     // if n is odd then there is n/2 even number and n/2 + 1 odd number 
             od=ev+1;
 
         }
 
+        // hence we find out the number of odd number and even number between 1 to n so we have to just use that formula 
         System.out.println(" Sum Of Even Number Is : " + ev*(ev+1)  + " \n Sum Of odd Number Is : " + od*od);
 
     }
--- SumOfEvenOdd.java
+++ SumOfEvenOdd.java
@@ -5,15 +5,16 @@ public class SumOfEvenOdd {
     public static void main(String[] args) {
         double n = 2000;
         int sumO = 0, sumE = 0;
+		// initalize the variable
 
-        for (int i=1;i<=Math.round(n);i++)
+        for (int i=1;i<=Math.round(n);i++) // this loop is to iterate from 1 to n
         {
             if(i%2==0)
             {
-                sumE=sumE+i;
+                sumE=sumE+i; // if number is even then add in sumE
             }else
             {
-                sumO=sumO+i;
+                sumO=sumO+i; // if number is odd then add in sumO
             }
         }
         System.out.println("Sum Of Even Number Is : " + sumE + " \n Sum Of odd Number Is " + sumO);
