677f9a39a0eb46bf4449b4e219890fb34bffb731
sahil mulla <sahilmulla199@gmail.com>
2020-06-28 17:02:18 +0530

    fix -ve and floating value

--- Odd_Even.java
+++ Odd_Even.java
@@ -4,20 +4,23 @@ import java.util.Scanner;
 
 public class Odd_Even {
     public static void main(String[] args) {
-        Scanner s=new Scanner(System.in);
+        Scanner s = new Scanner(System.in);
         System.out.println("Enter The Number : ");
-       int n=s.nextInt();
-        int ev=0,od=0;
-        if(n%2==0)
-        {
-          ev=n/2;
-          od= n/2;
-        }
-        else {
-            ev=n/2;
-            od=ev+1;
-        }
-        System.out.println(" Sum Of Even Number Is : " + ev*(ev+1)  + " \n Sum Of odd Number Is : " + od*od);
+        float n1 = s.nextFloat();
+        int n = Math.round(n1);
+        int ev = 0, od = 0;
+        if (n >= 0) {
+            if (n % 2 == 0) {
+                ev = n / 2;
+                od = n / 2;
+            } else {
+                ev = n / 2;
+                od = ev + 1;
+            }
+            System.out.println(" Sum Of Even Number Is : " + ev * (ev + 1) + " \n Sum Of odd Number Is : " + od * od);
 
+        }else {
+            System.out.println("enter positive number");
+        }
     }
 }
--- SumOfEvenOdd.java
+++ SumOfEvenOdd.java
@@ -4,22 +4,23 @@ import java.util.Scanner;
 
 public class SumOfEvenOdd {
     public static void main(String[] args) {
-        Scanner s=new Scanner(System.in);
+        Scanner s = new Scanner(System.in);
         System.out.println("Enter The Number :  ");
-        int n=s.nextInt();
+        float n1 = s.nextFloat();
+        int n = Math.round(n1);
         int sumO = 0, sumE = 0;
-
-        for (int i=1;i<=n;i++)
-        {
-            if(i%2==0)
-            {
-                sumE=sumE+i;
-            }else
-            {
-                sumO=sumO+i;
+        if (n >= 0) {
+            for (int i = 1; i <= n; i++) {
+                if (i % 2 == 0) {
+                    sumE = sumE + i;
+                } else {
+                    sumO = sumO + i;
+                }
             }
+            System.out.println(" Sum Of Even Number Is : " + sumE + " \n Sum Of odd Number Is " + sumO);
+        }else {
+            System.out.println("Enter The Positive Number ");
         }
-        System.out.println(" Sum Of Even Number Is : " + sumE + " \n Sum Of odd Number Is " + sumO);
     }
     }
 
