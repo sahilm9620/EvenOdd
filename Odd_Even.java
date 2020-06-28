f1b389e1f0f72b9e52401f74c4694ffdb7e3dbc9
sahil mulla <sahilmulla199@gmail.com>
2020-06-28 14:55:27 +0530

    typecast long to int

--- Odd_Even.java
+++ Odd_Even.java
@@ -2,24 +2,22 @@ package sahil.mulla.sahil;
 
 public class Odd_Even {
     public static void main(String[] args) {
-        int n=5,ev=0,od=0;
-		// there is formula for finding sum of first n odd number and even number
-		// for even = n*(n+1)  for odd=n*n
-		// so our first task is to find out how many even and odd numbers are there in between 1 to n 
-        if(n%2==0)
+        double n=2000;
+        int ev=0,od=0;
+        int n1= (int) Math.round(n);
+        if(n1%2==0)
         {
-          ev=n/2;
-          od=n/2;   // if n is even then there is n/2 even numbers and n/2 odd numbers 
+          ev=n1/2;
+          od= n1/2;
 
         }
         else {
 
-            ev=n/2;     // if n is odd then there is n/2 even number and n/2 + 1 odd number 
+            ev=n1/2;
             od=ev+1;
 
         }
 
-        // hence we find out the number of odd number and even number between 1 to n so we have to just use that formula 
         System.out.println(" Sum Of Even Number Is : " + ev*(ev+1)  + " \n Sum Of odd Number Is : " + od*od);
 
     }
--- SumOfEvenOdd.java
+++ SumOfEvenOdd.java
@@ -3,21 +3,19 @@ import java.lang.Math;
 
 public class SumOfEvenOdd {
     public static void main(String[] args) {
-        double n = 2000;
+        double n = 5.6;
         int sumO = 0, sumE = 0;
-		// initalize the variable
 
-        for (int i=1;i<=Math.round(n);i++) // this loop is to iterate from 1 to n
+        for (int i=1;i<=(int)Math.round(n);i++)
         {
             if(i%2==0)
             {
-                sumE=sumE+i; // if number is even then add in sumE
+                sumE=sumE+i;
             }else
             {
-                sumO=sumO+i; // if number is odd then add in sumO
+                sumO=sumO+i;
             }
         }
         System.out.println("Sum Of Even Number Is : " + sumE + " \n Sum Of odd Number Is " + sumO);
     }
     }
-
